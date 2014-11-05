6a0ddfbb0b4ca9c2113c9c014a461ed38c76151c
Fortisbellator <jobmetske@hotmail.com>
2014-11-05 16:40:49 +0100

    lulllesuyger

--- music_app/menu.py
+++ music_app/menu.py
@@ -1,6 +1,5 @@
 __author__ = 'MarkJan'
 def open_file():
-        def selectfile():
             lineEdit.setText(QFileDialog.getOpenFileName())
             open_library.triggered.connect(selectFile)
 def add_tags():
@@ -13,7 +12,7 @@ def convert_file_s():
     pass
 
 def create_playlist():
-    pass
+            create_playlist.triggered.
 
 def equalizer():
     pass
