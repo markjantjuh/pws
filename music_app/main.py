d38830c017ffabb965f383824129683494dab8f7
Mark Jan <markjanvanlieburg@gmail.com>
2014-11-05 17:51:36 +0100

    x

--- music_app/main.py
+++ music_app/main.py
@@ -4,12 +4,10 @@ __author__ = 'MarkJan'
 import sys
 
 from PyQt4 import QtGui
-import menu
-import player
+
 from music_app.user_interface.main_ui import Ui_File
 from user_interface.playlist_ui import Ui_playlist_dialog
-
-
+from user_interface.add_tags_ui import Ui_add_tags_dialog
 
 #application
 class Main(QtGui.QMainWindow):
@@ -22,25 +20,56 @@ class Main(QtGui.QMainWindow):
         #bind functions to UI
 
         #menubinds
-        self.ui.open_library.triggered.connect(menu.open_library)
-        self.ui.add_tags.triggered.connect(menu.add_tags)
-        self.ui.play_from_disc.triggered.connect(menu.play_from_disk)
-        self.ui.convert_files.triggered.connect(menu.convert_files)
+        self.ui.open_library.triggered.connect(self.open_library)
+        self.ui.add_tags.triggered.connect(self.add_tags)
+        self.ui.play_from_disc.triggered.connect(self.play_from_disk)
+        self.ui.convert_files.triggered.connect(self.convert_files)
         self.ui.create_playlist.triggered.connect(self.create_playlist)
-        self.ui.equalizer.triggered.connect(menu.equalizer)
+        self.ui.equalizer.triggered.connect(self.equalizer)
 
         #playerbinds
-        self.ui.Fastbackward.clicked.connect(player.fast_backward)
-        self.ui.Fastforward.clicked.connect(player.fast_forward)
-        self.ui.Shuffle.clicked.connect(player.shuffle)
-        self.ui.Play.clicked.connect(player.play_sound)
-        self.ui.Stop.clicked.connect(player.stop_sound)
+        self.ui.Fastbackward.clicked.connect(self.fast_backward)
+        self.ui.Fastforward.clicked.connect(self.fast_forward)
+        self.ui.Shuffle.clicked.connect(self.shuffle)
+        self.ui.Play.clicked.connect(self.play_sound)
+        self.ui.Stop.clicked.connect(self.stop_sound)
 
     def create_playlist(self):
-        self.new_window = Playlist_ui()
+        self.new_window = PlaylistUi()
         self.new_window.show()
 
-class Playlist_ui(QtGui.QMainWindow):
+    def add_tags(self):
+        self.new_window = AddTagsUi()
+        self.new_window.show()
+
+    def open_library(self):
+        pass
+
+    def play_from_disk(self):
+        pass
+
+    def convert_files(self):
+        pass
+
+    def equalizer(self):
+        pass
+
+    def fast_forward(self):
+        pass
+
+    def fast_backward(self):
+        pass
+
+    def shuffle(self):
+        pass
+
+    def play_sound(self):
+        pass
+
+    def stop_sound(self):
+        pass
+
+class PlaylistUi(QtGui.QMainWindow):
     def __init__(self):
         #initialize the UI
         QtGui.QMainWindow.__init__(self)
@@ -50,9 +79,14 @@ class Playlist_ui(QtGui.QMainWindow):
         self.ui.button_create.clicked.connect(self.plaalist)
 
     def plaalist(self):
-    #give input pls
-    #return plaalistnaampje
         print "DE FUNCTIE WERKT HOOR"
+
+class AddTagsUi(QtGui.QMainWindow):
+    def __init__(self):
+        #initialize the UI
+        QtGui.QMainWindow.__init__(self)
+        self.ui = Ui_add_tags_dialog()
+        self.ui.setupUi(self)
 
 #start application
 if __name__ == "__main__":
--- music_app/menu.py
+++ music_app/menu.py
@@ -1,34 +1,8 @@
 __author__ = 'MarkJan'
 
 from PyQt4 import QtGui
-
-
-
-
+from user_interface.playlist_ui import Ui_playlist_dialog
 
 '''def open_file():
     lineEdit.setText(QFileDialog.getOpenFileName())
     open_library.triggered.connect(selectFile) '''
-
-
-
-def add_tags():
-    pass
-
-def play_from_disk():
-    pass
-
-def convert_files():
-    pass
-
-def create_playlist():
-    ex = Playlist_ui()
-    ex.show()
-
-def equalizer():
-    pass
-
-def open_library():
-    pass
-
-
