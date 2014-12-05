783028aa8c6dbaf2e9ca67f0ac5bb3c055f2a394
Mark Jan <markjanvanlieburg@gmail.com>
2014-12-05 12:19:53 +0100

    x

--- music_app/main.py
+++ music_app/main.py
@@ -8,7 +8,7 @@ from music_app.user_interface.main_ui import Ui_File
 from user_interface.playlist_ui import Ui_playlist_dialog
 from user_interface.add_tags_ui import Ui_add_tags_dialog
 
-import playlist
+import playlist, music_library
 
 #application
 class Main(QtGui.QMainWindow):
@@ -41,7 +41,7 @@ class Main(QtGui.QMainWindow):
         #get input and call create_playlist
 
     def add_tags(self):
-        self.new_window = AddTagsUi()
+        self.new_window = AddTagsUi("x")
         self.new_window.show()
 
     def open_library(self):
@@ -84,6 +84,7 @@ class PlaylistUi(QtGui.QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.button_create.clicked.connect(self.createPlaylist)
+        self.ui.button_cancel.clicked.connect(self.close)
 
     def createPlaylist(self):
         self.name = str(self.ui.textbox_name.text())
@@ -92,11 +93,35 @@ class PlaylistUi(QtGui.QMainWindow):
         self.close()
 
 class AddTagsUi(QtGui.QMainWindow):
-    def __init__(self):
+    def __init__(self, filename):
         #initialize the UI
         QtGui.QMainWindow.__init__(self)
         self.ui = Ui_add_tags_dialog()
         self.ui.setupUi(self)
+
+        self.filename = filename
+
+        self.ui.button_save.clicked.connect(self.editTags)
+        self.ui.button_cancel.clicked.connect(self.close)
+
+    def editTags(self):
+        self.tag_title = str(self.ui.line_title.text())
+        self.tag_genre = str(self.ui.line_genre.text())
+        self.tag_artist = str(self.ui.line_artist.text())
+        self.tag_album = str(self.ui.line_album.text())
+        self.tag_year = str(self.ui.line_year.text())
+        self.tag_bpm = str(self.ui.line_bpm.text())
+        self.tag_key = str(self.ui.line_key.text())
+        self.tag_info = str(self.ui.line_info.text())
+
+        music_library.editTags(self.filename, 'Song Title', self.tag_title)
+        music_library.editTags(self.filename, 'Genre', self.tag_genre)
+        music_library.editTags(self.filename, 'Artist', self.tag_artist)
+        music_library.editTags(self.filename, 'Album', self.tag_album)
+        music_library.editTags(self.filename, 'Year', self.tag_year)
+        music_library.editTags(self.filename, 'BPM', self.tag_bpm)
+        music_library.editTags(self.filename, 'Key', self.tag_key)
+        music_library.editTags(self.filename, 'Info', self.tag_info)
 
 #start application
 if __name__ == "__main__":
@@ -104,4 +129,6 @@ if __name__ == "__main__":
     ex = Main()
     ex.show()
     sys.exit(app.exec_())
+
+#banaan
 
